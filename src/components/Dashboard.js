33715f541fc682f43d66ce0706372ad7ff1329b3
jackiestchen <jackiestchen@gmail.com>
2022-03-23 22:41:31 -0600

    Add render Panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
 class Dashboard extends Component {
   state = {
@@ -12,10 +36,23 @@ class Dashboard extends Component {
     const dashboardClasses = classnames("dashboard");
 
     if (this.state.loading) {
-      return <Loading />
+      return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    return (
+      <main className={dashboardClasses}>
+        {data.map((item) => {
+          return (
+            <Panel
+              key={item.id}
+              id={item.id}
+              label={item.lable}
+              value={item.value}
+            />
+          );
+        })}
+      </main>
+    );
   }
 }
 
