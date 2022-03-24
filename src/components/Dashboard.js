5ecefec130ffd2779c5c4beb803159a9062f7eb5
jackiestchen <jackiestchen@gmail.com>
2022-03-23 23:07:45 -0600

    Add onClick method to Panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,29 +30,42 @@ const data = [
 class Dashboard extends Component {
   state = {
     loading: false,
+    focused: null,
   };
 
+  selectPanel(id) {
+    this.setState({
+      focused: id,
+    });
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((item) => {
+        return (
+          <Panel
+            key={item.id}
+            id={item.id}
+            label={item.label}
+            value={item.value}
+            onSelect={this.selectPanel}
+          />
+        );
+      });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return (
-      <main className={dashboardClasses}>
-        {data.map((item) => {
-          return (
-            <Panel
-              key={item.id}
-              id={item.id}
-              label={item.lable}
-              value={item.value}
-            />
-          );
-        })}
-      </main>
-    );
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
