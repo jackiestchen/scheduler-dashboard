0edd5a22e3dd3abc32ee946948d2f18f6d77cecb
jackiestchen <jackiestchen@gmail.com>
2022-03-24 20:19:57 -0600

    Add fetch data method to component did mount

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,35 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6,
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm",
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday",
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3",
+    getValue: getInterviewsPerDay,
   },
 ];
 
@@ -42,6 +48,19 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -72,7 +91,7 @@ class Dashboard extends Component {
             key={item.id}
             id={item.id}
             label={item.label}
-            value={item.value}
+            value={item.getValue(this.state)}
             onSelect={(e) => this.selectPanel(item.id)}
           />
         );
